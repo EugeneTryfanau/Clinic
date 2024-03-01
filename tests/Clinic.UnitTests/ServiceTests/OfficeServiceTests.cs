9bd2b585230620cf15dca5029cb52b462fa654ff
EugeneTryfanau <eugenetryfanau@gmail.com>
2024-03-01 12:08:47 +0300

    returned xunit packages

--- tests/Clinic.UnitTests/ServiceTests/OfficeServiceTests.cs
+++ tests/Clinic.UnitTests/ServiceTests/OfficeServiceTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Clinic.UnitTests.ServiceTests
+﻿namespace Clinic.UnitTests.ServiceTests
 {
     public class OfficeServiceTests
     {
