0672cc06d3a1d8b7a53911f81cf9c714b2da3ffc
EugeneTryfanau <eugenetryfanau@gmail.com>
2024-03-06 14:36:56 +0300

    changed status in tests

--- tests/Clinic.IntegrationTests/TestData/Offices/TestOfficeViewModels.cs
+++ tests/Clinic.IntegrationTests/TestData/Offices/TestOfficeViewModels.cs
@@ -11,7 +11,7 @@ public class TestOfficeViewModels
         Id = new Guid("b37b1e25-8a6f-48dd-965b-0581911bb383"),
         Address = "Lenina",
         RegistryPhoneNumber = "375296666666",
-        IsActive = OfficeStatus.Active
+        IsActive = StandartStatus.Active
     };
 
     public static Office UpdateOfficeModel => new()
@@ -19,7 +19,7 @@ public class TestOfficeViewModels
         Id = new Guid("b37b1e25-8a6f-48dd-965b-0581911bb383"),
         Address = "Terehina",
         RegistryPhoneNumber = "375295555555",
-        IsActive = OfficeStatus.Active
+        IsActive = StandartStatus.Active
     };
 
     public static OfficeViewModel CreateOffice(Guid? id = null) => new()
@@ -27,6 +27,6 @@ public class TestOfficeViewModels
         Id = id is null ? Guid.NewGuid() : id.Value,
         Address = "Lenina",
         RegistryPhoneNumber = "375296666666",
-        IsActive = OfficeStatus.Active
+        IsActive = StandartStatus.Active
     };
 }
