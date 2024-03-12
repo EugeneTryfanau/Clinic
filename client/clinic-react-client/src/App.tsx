0587f8c7d4f0549188b3cb24471d8fd0cce3b06e
EugeneTryfanau <eugenetryfanau@gmail.com>
2024-03-12 07:12:11 +0100

    changes to understand how react works (will be changed/removed)

--- client/clinic-react-client/src/App.tsx
+++ client/clinic-react-client/src/App.tsx
@@ -1,56 +1,29 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
 
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
-
+    const [time, setTime] = useState(new Date());
+    
     useEffect(() => {
-        populateWeatherData();
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+        
+        return () => clearInterval(interval);
     }, []);
-
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tabelLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
+    
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    
+    const timeString = `${hours}:${minutes}:${seconds}`;
 
     return (
         <div>
-            <h1 id="tabelLabel">Weather forecast Ohayo</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
+            <h1>{timeString}</h1>
         </div>
     );
-
-    async function populateWeatherData() {
-        const response = await fetch('https://localhost:7056/api/Offices');
-        const data = await response.json();
-        setForecasts(data);
-    }
 }
 
 export default App;
