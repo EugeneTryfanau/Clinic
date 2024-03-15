94c55a4330d0727a1eda0a401e65c20b34125679
EugeneTryfanau <eugenetryfanau@gmail.com>
2024-03-15 12:28:00 +0100

    added crud requests to OfficeService, reworked OfficeStore to implement crud requests, added types for params

--- client/clinic-react-client/src/models/IStandartParams.ts
+++ client/clinic-react-client/src/models/IStandartParams.ts
@@ -2,3 +2,15 @@ interface StandartParams {
     limit?: number,
     page?: number
 }
+
+interface StandartParamsWithId extends StandartParams{
+    id: string
+}
+
+interface StandartParamsWithEntity<T> extends StandartParams{
+    entity: T
+}
+
+interface StandartParamsWithIdAndEntity<T> extends StandartParamsWithId{
+    entity: T
+}
--- client/clinic-react-client/src/store/officeStore.ts
+++ client/clinic-react-client/src/store/officeStore.ts
@@ -1,23 +1,66 @@
 import { StateCreator } from 'zustand'
 import { IOffice } from '../models/IOffice';
-import { getOffices } from '../services/OffisesService';
+import { createOffice, deleteOffice, getByIdOffice, getOffices, updateOffice } from '../services/OffisesService';
 
 export interface OfficeSlice {
-    offices: IOffice[]
+    offices: IOffice[],
+    office: IOffice,
     isLoading: boolean
     errors: string[]
     fetchOffices: () => Promise<void>
+    getOfficeById: (id: string) => Promise<void>
+    createOffice: (office: IOffice) => Promise<void>
+    updateOffice: (id: string, office: IOffice) => Promise<void>
+    deleteOffice: (id: string) => Promise<void>
 }
 
-export const getOfficeSlice: StateCreator<OfficeSlice> = (set) => ({
+export const getOfficeSlice: StateCreator<OfficeSlice> = (set, get) => ({
     offices: [],
+    office: {} as IOffice,
     isLoading: false,
     errors: [],
     fetchOffices: async () => {
+        set({ isLoading: true });
         const response = await getOffices();
         response.code === "error" ? 
-            set({ errors: [response.code, response.error.message] }) 
+            set({ errors: [response.code, response.error.message], isLoading: false }) 
             : 
-            set({ offices: response.data });
+            set({ offices: response.data, isLoading: false });
+    },
+    getOfficeById: async (id: string) => {
+        set({ isLoading: true });
+        const response = await getByIdOffice({id});
+        if (response.code === "error") {
+            set({ errors: [response.code, response.error.message], isLoading: false });
+        } else {
+            set({ office: response.data, isLoading: false });
+        }
+    },
+    createOffice: async (office: IOffice) => {
+        set({ isLoading: true });
+        const response = await createOffice({entity: office});
+        if (response.code === "error") {
+            set({ errors: [response.code, response.error.message], isLoading: false });
+        } else {
+            set({ offices: [...get().offices, response.data], office: response.data, isLoading: false });
+        }
+    },
+    updateOffice: async (id: string, office: IOffice) => {
+        set({ isLoading: true });
+        const response = await updateOffice({id: id, entity: office});
+        if (response.code === "error") {
+            set({ errors: [response.code, response.error.message], isLoading: false });
+        } else {
+            set({ offices: get().offices.map(o => o.id === id ? response.data : o), office: response.data, isLoading: false });
+        }
+    },
+    deleteOffice: async (id: string) => {
+        set({ isLoading: true });
+        const response = await deleteOffice({id: id});
+        if (response.code === "error") {
+            set({ errors: [response.code, response.error.message], isLoading: false });
+        } else {
+            set({ offices: get().offices.filter(o => o.id !== id), office: {} as IOffice, isLoading: false });
+        }
     }
 })
