0aaf0628afce04a55919a706d105a2cc2232fb5e
EugeneTryfanau <eugenetryfanau@gmail.com>
2024-04-02 12:25:19 +0200

    added mapping profile to bll = dal entities

--- src/Profiles/Profiles.API/Mapper/MapperProfile.cs
+++ src/Profiles/Profiles.API/Mapper/MapperProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Profiles.BLL.Models;
+using Profiles.DAL.Entities;
 
 namespace Profiles.API.Mapper
 {
@@ -6,7 +8,11 @@ namespace Profiles.API.Mapper
     {
         public MapperProfile()
         {
-            
+            CreateMap<AccountEntity, Account>().ReverseMap();
+            CreateMap<DoctorEntity, Doctor>().ReverseMap();
+            CreateMap<PatientEntity, Patient>().ReverseMap();
+            CreateMap<ReceptionistEntity, Receptionist>().ReverseMap();
+            CreateMap<SpecializationEntity, Specialization>().ReverseMap();
         }
     }
 }
