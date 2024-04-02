659902688c585e9c2dba24c4e8a21091d928b8cb
EugeneTryfanau <eugenetryfanau@gmail.com>
2024-04-02 14:20:08 +0200

    added default controllers with crud for each entity in Profiles

--- src/Profiles/Profiles.API/Controllers/AccountController.cs
+++ src/Profiles/Profiles.API/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Profiles.API.ViewModels.Account;
 using Profiles.BLL.Interrfaces;
+using Profiles.BLL.Models;
 
 namespace Profiles.API.Controllers
 {
@@ -15,9 +16,42 @@ namespace Profiles.API.Controllers
         [HttpGet]
         public async Task<IEnumerable<AccountViewModel>> GetAll(string? email, string? phoneNumber, bool? isActive, CancellationToken cancellationToken)
         {
-            var offices = await _accountService.GetAllAsync(email, phoneNumber, isActive, cancellationToken);
+            var accounts = await _accountService.GetAllAsync(email, phoneNumber, isActive, cancellationToken);
 
-            return _mapper.Map<IEnumerable<AccountViewModel>>(offices);
+            return _mapper.Map<IEnumerable<AccountViewModel>>(accounts);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<AccountViewModel> GetById(Guid id, CancellationToken cancellationToken)
+        {
+            var account = await _accountService.GetByIdAsync(id, cancellationToken);
+
+            return _mapper.Map<AccountViewModel>(account);
+        }
+
+        [HttpPost]
+        public async Task<AccountViewModel> Create(CreateAccountViewModel viewModel, CancellationToken cancellationToken)
+        {
+            var account = _mapper.Map<Account>(viewModel);
+            var result = await _accountService.CreateAsync(account, cancellationToken);
+
+            return _mapper.Map<AccountViewModel>(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<AccountViewModel> Update(Guid id, UpdateAccountViewModel viewModel, CancellationToken cancellationToken)
+        {
+            var modelToUpdate = _mapper.Map<Account>(viewModel);
+            modelToUpdate.Id = id;
+            var result = await _accountService.UpdateAsync(modelToUpdate, cancellationToken);
+
+            return _mapper.Map<AccountViewModel>(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(Guid id, CancellationToken cancellationToken)
+        {
+            await _accountService.DeleteAsync(id, cancellationToken);
         }
     }
 }
---
+++ src/Profiles/Profiles.API/Controllers/DoctorController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Profiles.API.ViewModels.Doctor;
+using Profiles.BLL.Interrfaces;
+using Profiles.BLL.Models;
+using Profiles.DAL.Entities;
+
+namespace Profiles.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorController(IDoctorService doctorService, IMapper mapper) : ControllerBase
+    {
+        private readonly IDoctorService _doctorService = doctorService;
+        private readonly IMapper _mapper = mapper;
+
+        [HttpGet]
+        public async Task<IEnumerable<DoctorViewModel>> GetAll(string? name, DoctorStatus? status, CancellationToken cancellationToken)
+        {
+            var doctors = await _doctorService.GetAllAsync(name, status, cancellationToken);
+
+            return _mapper.Map<IEnumerable<DoctorViewModel>>(doctors);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<DoctorViewModel> GetById(Guid id, CancellationToken cancellationToken)
+        {
+            var doctor = await _doctorService.GetByIdAsync(id, cancellationToken);
+
+            return _mapper.Map<DoctorViewModel>(doctor);
+        }
+
+        [HttpPost]
+        public async Task<DoctorViewModel> Create(CreateDoctorViewModel viewModel, CancellationToken cancellationToken)
+        {
+            var doctor = _mapper.Map<Doctor>(viewModel);
+            var result = await _doctorService.CreateAsync(doctor, cancellationToken);
+
+            return _mapper.Map<DoctorViewModel>(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<DoctorViewModel> Update(Guid id, UpdateDoctorViewModel viewModel, CancellationToken cancellationToken)
+        {
+            var modelToUpdate = _mapper.Map<Doctor>(viewModel);
+            modelToUpdate.Id = id;
+            var result = await _doctorService.UpdateAsync(modelToUpdate, cancellationToken);
+
+            return _mapper.Map<DoctorViewModel>(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(Guid id, CancellationToken cancellationToken)
+        {
+            await _doctorService.DeleteAsync(id, cancellationToken);
+        }
+    }
+}
