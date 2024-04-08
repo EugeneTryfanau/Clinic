eab9f1b85d58fc367722244b3349445a8d098e09
EugeneTryfanau <eugenetryfanau@gmail.com>
2024-04-08 14:21:00 +0200

    created baseController

--- src/Appointments/Appointments.API/Controllers/AppointmentController.cs
+++ src/Appointments/Appointments.API/Controllers/AppointmentController.cs
@@ -4,15 +4,16 @@ using Appointments.BLL.Interfaces;
 using Appointments.BLL.Models;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using StandartCRUD.StandartAPI.Controllers;
 
 namespace Appointments.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AppointmentController(IAppointmentService appointmentService, IMapper mapper) : ControllerBase
+    public class AppointmentController(IAppointmentService appointmentService, IMapper mapper) : 
+        BaseController<Appointment,AppointmentViewModel, CreateAppointmentViewModel, UpdateAppointmentViewModel>(appointmentService, mapper)
     {
         private readonly IAppointmentService _appointmentService = appointmentService;
-        private readonly IMapper _mapper = mapper;
 
         [HttpGet]
         public async Task<IEnumerable<AppointmentViewModel>> GetAll([FromQuery] AppointmentSearchRequestData requestData, CancellationToken cancellationToken)
@@ -20,39 +21,6 @@ namespace Appointments.API.Controllers
             var appointments = await _appointmentService.GetAllAsync(requestData.PatientId, requestData.DoctorId, requestData.ServiceId, requestData.IsApproved, cancellationToken);
 
             return _mapper.Map<IEnumerable<AppointmentViewModel>>(appointments);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<AppointmentViewModel> GetById(Guid id, CancellationToken cancellationToken)
-        {
-            var appointment = await _appointmentService.GetByIdAsync(id, cancellationToken);
-
-            return _mapper.Map<AppointmentViewModel>(appointment);
-        }
-
-        [HttpPost]
-        public async Task<AppointmentViewModel> Create(CreateAppointmentViewModel viewModel, CancellationToken cancellationToken)
-        {
-            var appointment = _mapper.Map<Appointment>(viewModel);
-            var result = await _appointmentService.CreateAsync(appointment, cancellationToken);
-
-            return _mapper.Map<AppointmentViewModel>(result);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<AppointmentViewModel> Update(Guid id, UpdateAppointmentViewModel viewModel, CancellationToken cancellationToken)
-        {
-            var modelToUpdate = _mapper.Map<Appointment>(viewModel);
-            modelToUpdate.Id = id;
-            var result = await _appointmentService.UpdateAsync(modelToUpdate, cancellationToken);
-
-            return _mapper.Map<AppointmentViewModel>(result);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task Delete(Guid id, CancellationToken cancellationToken)
-        {
-            await _appointmentService.DeleteAsync(id, cancellationToken);
         }
     }
 }
--- src/Appointments/Appointments.API/Controllers/ResultController.cs
+++ src/Appointments/Appointments.API/Controllers/ResultController.cs
@@ -4,15 +4,16 @@ using Appointments.BLL.Interfaces;
 using Appointments.BLL.Models;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using StandartCRUD.StandartAPI.Controllers;
 
 namespace Appointments.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ResultController(IResultService resultService, IMapper mapper) : ControllerBase
+    public class ResultController(IResultService resultService, IMapper mapper) :
+        BaseController<Result, ResultViewModel, CreateResultViewModel, UpdateResultViewModel>(resultService, mapper)
     {
         private readonly IResultService _resultService = resultService;
-        private readonly IMapper _mapper = mapper;
 
         [HttpGet]
         public async Task<IEnumerable<ResultViewModel>> GetAll([FromQuery] AppointmentSearchRequestData requestData, CancellationToken cancellationToken)
@@ -20,39 +21,6 @@ namespace Appointments.API.Controllers
             var results = await _resultService.GetAllAsync(requestData.AppountmentId, cancellationToken);
 
             return _mapper.Map<IEnumerable<ResultViewModel>>(results);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ResultViewModel> GetById(Guid id, CancellationToken cancellationToken)
-        {
-            var result = await _resultService.GetByIdAsync(id, cancellationToken);
-
-            return _mapper.Map<ResultViewModel>(result);
-        }
-
-        [HttpPost]
-        public async Task<ResultViewModel> Create(CreateResultViewModel viewModel, CancellationToken cancellationToken)
-        {
-            var result = _mapper.Map<Result>(viewModel);
-            var createResult = await _resultService.CreateAsync(result, cancellationToken);
-
-            return _mapper.Map<ResultViewModel>(createResult);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<ResultViewModel> Update(Guid id, UpdateResultViewModel viewModel, CancellationToken cancellationToken)
-        {
-            var modelToUpdate = _mapper.Map<Result>(viewModel);
-            modelToUpdate.Id = id;
-            var result = await _resultService.UpdateAsync(modelToUpdate, cancellationToken);
-
-            return _mapper.Map<ResultViewModel>(result);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task Delete(Guid id, CancellationToken cancellationToken)
-        {
-            await _resultService.DeleteAsync(id, cancellationToken);
         }
     }
 }
