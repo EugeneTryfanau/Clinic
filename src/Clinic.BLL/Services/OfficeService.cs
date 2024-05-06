5e3390332218a2e3bbd496644cc7de2067fce9a8
EugeneTryfanau <eugenetryfanau@gmail.com>
2024-05-06 10:09:43 +0200

    addede default inMemory caching on GetAllAsync request

--- src/Clinic.BLL/Services/OfficeService.cs
+++ src/Clinic.BLL/Services/OfficeService.cs
@@ -3,20 +3,31 @@ using Clinic.BLL.Interfaces;
 using Clinic.BLL.Models;
 using Clinic.DAL.Entities;
 using Clinic.DAL.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
 using StandartCRUD;
 using StandartCRUD.StandartBLL;
 
 namespace Clinic.BLL.Services
 {
-    public class OfficeService(IOfficeRepository officeRepository, IMapper mapper) : 
-        GenericService<OfficeEntity, Office>(officeRepository, mapper), 
+    public class OfficeService(IOfficeRepository officeRepository, IMapper mapper, IMemoryCache cache) :
+        GenericService<OfficeEntity, Office>(officeRepository, mapper),
         IOfficeService
     {
+        private readonly IMemoryCache _cache = cache;
+
         public async Task<IEnumerable<Office>> GetAllAsync(string? address, string? phoneNumber, StandartStatus? isActive, CancellationToken cancellationToken)
         {
-            var offices = await officeRepository.GetAllAsync(address, phoneNumber, isActive, cancellationToken);
 
-            return _mapper.Map<IEnumerable<Office>>(offices);
+            var cacheKey = $"Offices_{address}_{phoneNumber}_{isActive}";
+            var cachedOffices = await _cache.GetOrCreateAsync(cacheKey, async entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
+                var offices = await officeRepository.GetAllAsync(address, phoneNumber, isActive, cancellationToken);
+
+                return _mapper.Map<IEnumerable<Office>>(offices);
+            });
+
+            return cachedOffices!;
         }
     }
 }
